f49d1f6c305f74a4eb09806d1c861ccf3ea3f97b
Wojecki <131776716+Wojecki@users.noreply.github.com>
2025-02-13 16:12:00 +0100

    Add files via upload

---
+++ Lab4_z1/Rectangle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4z1
+{
+    internal class Rectangle: Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine($"Rysunke prostokąta o szerokości {Width} i wysokości {Height}");
+        }
+    }
+}
---
+++ Lab4_z1/Triangle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4z1
+{
+    internal class Triangle :Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine($"Rysunek trójkąta o wysokości {Height} i szerokości {Width}");
+        }
+    }
+}
